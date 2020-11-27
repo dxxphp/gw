445a97266e3d7f8db5977a1f1cafd7172a6c8c74
duxinxin <1143122018@qq.com>
2020-11-27 16:14:48 +0800

    数据库修改

--- mysql.php
+++ mysql.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'DB_TYPE' => 'mysqli',
-  'DB_HOST' => 'localhost',
+  'DB_HOST' => '39.106.224.192',
   'DB_NAME' => 'gw',
   'DB_USER' => 'root',
-  'DB_PWD' => 'root',
+  'DB_PWD' => '123456',
   'DB_PORT' => '3306',
   'DB_PREFIX' => 'gw_',
 );
